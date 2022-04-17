2590ed3fc4dd2637ef913775d08f766bcca5581e
Dmitriy Rostovskiy <rostovskiy.dm@gmail.com>
2022-04-17 22:24:09 +0300

    Add the task №3

--- src/main/java/ru/gb/rostovskiy/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/rostovskiy/homework1/HomeWorkApp.java
@@ -2,6 +2,16 @@ package ru.gb.rostovskiy.homework1;
 
 public class HomeWorkApp {
 
+    public static void checkSumSign(){
+        int a = -2300;
+        int b = 987;
+        if ((a + b) > 0){
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
     public static void printThreeWords(){
         System.out.println("Orange");
         System.out.println("Banana");
@@ -10,6 +20,8 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
+
     }
 
 }
