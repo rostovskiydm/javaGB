0fd81b4cc303911acb323865c319cd29967e33ab
Dmitriy Rostovskiy <rostovskiy.dm@gmail.com>
2022-04-23 18:33:54 +0300

    Add the task 3

--- src/main/java/ru/gb/rostovskiy/Homework3.java
+++ src/main/java/ru/gb/rostovskiy/Homework3.java
@@ -8,9 +8,9 @@ public class Homework3 {
     public static void main(String[] args) {
         firstTaskSolution();
         secondTaskSolution();
+        thirdTaskSolution();
 
     }
-
     public static void firstTaskSolution() {
         System.out.println("Решение задания №1");
         int[] arr = new int[15];
@@ -25,17 +25,27 @@ public class Homework3 {
                 arr[i] = 0;
             }
         }
-        System.out.println("Массив после подмены 0 на 1 и наоборот: \n" + Arrays.toString(arr));
+        System.out.println("Массив после подмены 0 на 1 и наоборот: \n" + Arrays.toString(arr) + "\n");
     }
 
     public static void secondTaskSolution() {
         System.out.println("Решение задания №2");
         int[] arr = new int[100];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i;
+        for (int i = 1; i <= arr.length; i++) {
+            arr[i-1] = i;
         }
-        System.out.println("Полученный массив: \n" + Arrays.toString(arr));
+        System.out.println("Полученный массив: \n" + Arrays.toString(arr) + "\n");
     }
 
+    public static void thirdTaskSolution(){
+        System.out.println("Решение задания №3");
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6){
+                arr[i] *= 2;
+            }
+        }
+        System.out.println("Получившийся массив: \n" + Arrays.toString(arr) + "\n");
+    }
 }
 
